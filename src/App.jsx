6c7bf2b2f31ee51183d295d958ae6a9b4a053d3d
Piyush Goswami <143679120+piyushg3803@users.noreply.github.com>
2024-12-13 17:50:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import About from "./components/About";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 import HeroSection from "./components/HeroSection";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
